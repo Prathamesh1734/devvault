7443d14dd06e9406e0aa506647ec011e744322fa
0ffline <prathameshppakhare@gmail.com>
2026-06-11 12:20:52 +0530

    fixed minor error in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,18 @@ function App() {
   const [activeCategoryId, setActiveCategoryId] = useState(allCategories[0].id);
   const activeCategory = allCategories.find((c) => c.id === activeCategoryId);
 
-  return;
-  <div className="flex h-screen bg-white text-gray-800 font-sans">
-    <Sidebar
-      categories={allCategories}
-      activeId={activeCategoryId}
-      onSelect={setActiveCategoryId}
-    ></Sidebar>
-    <main className="flex-1 overflow-y-auto p-10">
-      <ContentPanel category={activeCategory}></ContentPanel>
-    </main>
-  </div>;
+  return (
+    <div className="flex h-screen bg-white text-gray-800 font-sans">
+      <Sidebar
+        categories={allCategories}
+        activeId={activeCategoryId}
+        onSelect={setActiveCategoryId}
+      ></Sidebar>
+      <main className="flex-1 overflow-y-auto p-10">
+        <ContentPanel category={activeCategory}></ContentPanel>
+      </main>
+    </div>
+  );
 }
 
 export default App;
