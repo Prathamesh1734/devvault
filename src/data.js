af69b36a44d3a76957e4431ade64ee8fc831884a
0ffline <prathameshppakhare@gmail.com>
2026-06-13 12:43:18 +0530

    added few more fundamental questions for react

--- src/data.js
+++ src/data.js
@@ -1,7 +1,15 @@
 import reactData from "./react_interview_qa.json";
 import generalData from "./interview_qa.json";
+import reactJsData from "./react_js_qa.json";
+
+const reactFundamentalData = {
+  id: "fundamentals",
+  label: reactJsData.category,
+  questions: reactJsData.questions,
+};
 
 export const allCategories = [
-  ...reactData.categories,
+  reactFundamentalData,
   ...generalData.categories,
+  ...reactData.categories,
 ];
